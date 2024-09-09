30b4cdeaf37029ea11775ac4da5c7816e593cce1
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2024-09-09 23:35:29 +0200

    Update users.py

--- src/rag_api/api/endpoints/users.py
+++ src/rag_api/api/endpoints/users.py
@@ -9,6 +9,7 @@ from rag_api.utils.exceptions import UserExistsException, UserDoesNotExist
 
 
 logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 router = APIRouter()
 
